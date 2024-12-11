a3e2f59b955bd55ae41868b22fa18d65c56283b8
Mete <metlandus@hotmail.com.tr>
2024-12-11 16:51:02 +0300

    Formik, yup and nanoid added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react"
+import { Formik, Field, Form } from 'formik';
+
 import ContactForm from "./Components/ContactForm"
 import SearchBox from "./Components/SearchBox"
 import ContactList from "./Components/ContactList"
 import contactsData from "./data.json"
+
 function App() {
 
     const [searchedContact, setSearchedContact] = useState("")
     const [contact, setContact] = useState(contactsData);
-
     function handleChange(e) {
         const searchValue = e.target.value.toLowerCase();
         setSearchedContact(searchValue);
@@ -22,15 +24,26 @@ function App() {
         }
     }
 
-
     return (
         <>
             <h1>Phonebook</h1>
+            <Formik
+                initialValues={{
+                    name: '',
+                    phoneNumber: '',
+                }}
+                onSubmit={async (values) => {
+                    await new Promise((r) => setTimeout(r, 500));
+                    alert(JSON.stringify(values, null, 2));
+                }}
+            >
             <ContactForm />
+            </Formik>
             <SearchBox onChange={handleChange} />
             <ContactList contacts={contact} />
         </>
     )
 }
+
 
 export default App
--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Formik, Field, Form } from 'formik';
+import { nanoid } from 'nanoid'
+
 
 const ContactForm = () => {
     return (
-        <div>ContactForm</div>
+        <Form>
+            <label htmlFor="name">Name</label>
+            <Field id="name" name="name" placeholder="Jane Doe" />
+
+            <label htmlFor="number">Number</label>
+            <Field
+                id="number"
+                name="number"
+                placeholder="jane@acme.com"
+            />
+            <button type="submit">Add Contact</button>
+        </Form>
     )
 }
 
