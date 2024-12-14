51cec036bccb40b6c87102bc97443b47c7b457e2
Mete <metlandus@hotmail.com.tr>
2024-12-15 00:55:35 +0300

    Yup functionality added

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { nanoid } from 'nanoid'
 import contactsData from "../data.json"
+import * as yup from 'yup';
 
+const validation = yup.object().shape({
+    name: yup.string().min(3).max(50).required("required"),
+    number: yup.number().required("required")
+})
 
 const ContactForm = ({ addContact }) => {
     const handleSubmit = (values, { resetForm }) => {
         const newContact = {
-            id: nanoid(),
+            id: nanoid(4),
             name: values.name,
             number: values.number,
         };
         addContact(newContact);
         resetForm();
     }
+    
 
     return (
         <Formik
@@ -22,17 +28,20 @@ const ContactForm = ({ addContact }) => {
                 name: '',
                 number: '',
             }}
+            validationSchema={validation}
             onSubmit={handleSubmit}
         >
             <Form>
                 <label htmlFor="name">Name</label>
                 <Field id="name" name="name" placeholder="Jane Doe" />
+                <ErrorMessage name="name" component="div" className="error" />
                 <label htmlFor="number">Number</label>
                 <Field
                     id="number"
                     name="number"
                     placeholder="jane@acme.com"
                 />
+                <ErrorMessage name="number" component="div" className="error" />
                 <button type="submit">Add Contact</button>
             </Form>
         </Formik>
